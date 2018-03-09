c426a2d852d9aed392bca0755ca46a046fc2a4e0
wally.chang <wally.chang@protolabs.co.jp>
2018-03-09 17:52:29 +0900

    init commit of webpack vue page

---
+++ webpack/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<v-app id="app">
+  <app-header></app-header>
+  <v-container>
+    Container
+  </v-container>
+  <router-view/>
+</v-app>    
+</template>
+
+<script>
+import Vuetify from 'vuetify'
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  name: 'App',
+  components:{
+    'app-header': AppHeader
+  },
+}
+</script>
+
---
+++ webpack/src/components/MainContent.vue
@@ -0,0 +1,20 @@
+<template>
+<div id='mainContent'></div>
+</template>
+<script>
+export default {
+  name: 'MainContent',
+  data () {
+    return {
+      msg: 'Hi'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#mainContent{
+  height: 1000px;
+}
+</style>
